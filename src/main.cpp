a14da2cd33bd4c558ba1214a6c0d71b6d64c09d7
yygzdnl <125798737+yygzdnl@users.noreply.github.com>
2024-01-26 09:09:13 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,10 @@ int main(){
 int given_arr[2][3]{{1,2,3},{4,5,6}};
 Tensor t1=Tensor(given_arr);
 print;
+/*
+the given data as a argument so we'll just make 
+a new tensor and copy data on it.
+*/
 //2.given-shape and randomly
 int shape[]{2,3,4};
 t1=rand<double>(shape);
@@ -39,17 +43,24 @@ print;
 
 //4.initial with ones
 int shape2[]{2,2};
+Tensor t=ts::ones<int>(shape2);
+cout<<"vec for all ones"<<t<<endl;
 /*
 得到的是全1的tensor
 shape可自定
-
 */
 
 
 //5.initial with given value
+int shape4[2]{2,2};
+int given_value[2][2]{{1,2},{3,4}};
 
 
+cout<<"initial_with_given_arr"<<t<<endl;
 
+/*
+make a new tensor with given shape and data type
+*/
 
 //6.pattern
 
@@ -159,14 +170,15 @@ cout<<"data_sharing_same_menmory?"<<(&(t1.data_ptr()[0])==&(t1.transpose(0,1).da
 //second-part-end
    
 //thrid-part begin
-int arr[2][3]{{1,2,3},{2,3,4}};
+int arr[2]{2,3};
 int arr2[2][3]{{0,0,1},{2,1,0}};
-
-t1=Tensor(arr);
-Tensor t2=Tensor(arr2);
+//1.add
+t1=ts::ones<int>(arr);
+Tensor t2=ts::ones<int>(arr);
 
 cout<<"tensor 1"<<t1<<endl;
 cout<<"tensor 2"<<t2<<endl;
+
 cout<<"+:"<<t1+t2<<endl;
 cout<<"add"<<add(t1,t2)<<endl;
 
@@ -174,6 +186,70 @@ cout<<"add"<<add(t1,t2)<<endl;
 overwritting operator + 
 and the method add
 and static method add is aslo ok
+*/
+//2.sub
+cout<<"-:"<<t1-t2<<endl;
+cout<<"sub"<<endl<<sub(t1,t2)<<endl;
+
+//3.div
+cout<<"div"<<endl<<t1.div_pt(t2)<<endl;
+
+
+//4.mul_pt
+cout<<"mul_pt"<<endl<<t1.mul_pt(t2)<<endl;
+
+//5.mul_dot
+t1=rand<int>(arr);
+int arr_new[2]{3,1};
+t2=rand<int>(arr_new);
+cout<<"mul_dot"<<endl<<t1.mul_dot(t2)<<endl;
+
+/*
+all thie method have to check the shape
+and the point_wise-method must have same shape
+while mul_dot must have shape that can mtach
+*/
+
+
+//1.ge
+t1=rand<int>(arr);
+t2=rand<int>(arr);
+cout<<"tensor1:"<<endl<<t1<<endl;
+cout<<"tensor2:"<<endl<<t2<<endl;
+cout<<"tensor>=tensor2:"<<endl<<t1.ge(t2)<<endl;
+
+//2.le
+cout<<"tensor<=tensor2:"<<endl<<t1.le(t2)<<endl;
+
+//3.eq
+cout<<"tensor==tensor2:"<<endl<<t1.eq(t2)<<endl;
+
+//4.gt
+cout<<"tensor>tensor2:"<<endl<<t1.gt(t2)<<endl;
+
+//5.lt
+cout<<"tensor>=tensor2:"<<endl<<t1.lt(t2)<<endl;
+/*
+compare with each other point-wise
+*/
+
+
+//max
+cout<<"tensor:"<<endl<<t1<<endl;
+cout<<"max:"<<endl<<max(t1,0,false)<<endl;
+
+//min
+cout<<"min:"<<endl<<min(t1,0,false)<<endl;
+
+//mean
+cout<<"mean:"<<endl<<t1.mean(0,false)<<endl;
+
+//sum
+cout<<"sum:"<<endl<<t1.sum(0,false)<<endl;
+
+/*
+these method all can choose keep-dim or not
+and here we use false means do not keep dimension
 */
 
 
